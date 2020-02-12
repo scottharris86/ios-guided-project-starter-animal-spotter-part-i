335162cca6ba9521a2f1df710f06ba37cf46ff90
scottharris86 <scottharrisdev@gmail.com>
2020-02-12 12:58:47 -0500

    set up signUp endpoint method and http request

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -20,6 +20,40 @@ class APIController {
     
     // create function for sign up
     
+    func signUp(with user: User, completion: @escaping (Error?) -> Void) {
+        // create endpoint-specific URL
+        let signUpURL = baseUrl.appendingPathComponent("users/signup")
+        
+        // create url request from above
+        var request = URLRequest(url: signUpURL)
+        
+        // modify the request for POST, add proper headers
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // encode user model to JSON, attach as request body
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+            
+        } catch {
+            NSLog("Error encoding user object \(error)")
+            completion(error)
+            return
+        }
+        
+        // set up data task and handle response
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let error = error {
+                
+            }
+            
+        }.resume()
+        
+        
+    }
+    
     // create function for sign in
     
     // create function for fetching all animal names
---
+++ AnimalSpotter/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  AnimalSpotter
+//
+//  Created by scott harris on 2/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+    
+}
